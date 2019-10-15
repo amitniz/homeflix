68b94cecab0c10c8c93a3d33368cae713bf91d5f
Amit Nizri <amitnzri@gmail.com>
2019-10-15 22:59:30 +0300

    commit

---
+++ dataBase/updater.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+import os
+
+LOCATION = 'demo'
+DATABASE = 'homeFlix'
+HOST = 'localhost'
+PORT = 27017
+
+client = MongoClient(HOST,PORT)
+db = client[DATABASE]
+
+types = ['movies','series']
+genres = ['action','comedy','thriller','drama','adventure']
+
+
+def name(item):
+    return item.replace('_',' ').title()
+
+print "----------------------------\n[+] Updating the database..\n----------------------------"
+for t in types:
+    for g in genres:
+        item_list = os.listdir(os.path.join(LOCATION,t,g))
+        for item in item_list:
+            src = os.path.join(LOCATION,t,g,item)
+            if t == 'series':
+                seasons = [s for s in os.listdir(os.path.join(LOCATION,t,g,item)) if os.path.isdir(os.path.join(LOCATION,t,g,item,s))]
+                num_of_episodes = 0
+                for s in seasons:
+                    num_of_episodes+= len(os.listdir(os.path.join(LOCATION,t,g,item,s)))
+
+                sub_dirs = dict(zip(['src','location','genre','name','seasons','episodes'],
+                    [src,LOCATION,g,name(item),len(seasons),num_of_episodes]))
+                if db.series.find({"name":item}).count()==0:
+                    db.series.insert_one(sub_dirs)
+                    print "[+] Updated: ", sub_dirs
+            else:
+                sub_dirs = dict(zip(['src','location','genre','name'],[src,LOCATION,g,name(item)]))
+                if db.movies.find({"name":item}).count()==0:
+                    db.movies.insert_one(sub_dirs)
+                    print "[+] Updated: ", sub_dirs
+print "[+] Done.."
